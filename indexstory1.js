e533917d1b93260cef389ca54889744a7f8e7684
lejodev <alejandro.rv97@gmail.com>
2023-01-23 18:14:04 -0500

    1st story (start)

---
+++ indexstory1.js
@@ -0,0 +1,13 @@
+const RobertMoney = prompt("Robert. How many money do you hace?");
+const PedroMoney = prompt("Pedro. How many money do you hace?");
+const CoflaMoney = prompt("Cofla. How many money do you hace?");
+
+const prices = {
+  waterIceCream: 0.6,
+  creamyIceCream: 1,
+  iceCreamBrand1: 1.6,
+  iceCreamBrand2: 1.7,
+  iceCreamBrand3: 1.8,
+  confettiIceCreamJar: 2.9,
+  quarterKiloIceCream: 2.9,
+};
